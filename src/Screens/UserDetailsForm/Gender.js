c6d17d6fc3c65b402bb95debe16c963c5eb0f770
ullas-kiran <ullas261@gmail.com>
2023-05-11 15:56:37 +0530

    gender page completed

--- src/Screens/UserDetailsForm/Gender.js
+++ src/Screens/UserDetailsForm/Gender.js
@@ -1,6 +1,12 @@
 import { View, Text,StyleSheet,Button,TouchableOpacity,TextInput,Pressable,Image } from 'react-native'
 import {useState} from 'react'
 const Gender = () => {
+
+  const [selectedGender,setSelectedGender]=useState("");
+
+  const handleClick=(gender)=>{
+    setSelectedGender(gender)
+  }
   return (
     <View style={styles.container}>
       <Text  style={{  fontSize: 50,fontWeight: 'bold',textAlign:'center',color:'black',marginRight:'50%',marginTop:'40%'}}>I am</Text>
@@ -9,20 +15,24 @@ const Gender = () => {
           source={require('../../Assets/Images/gender-diversity.png')}
       />
         <View style={{flexDirection: "row", justifyContent: "space-between",margin:'10%'}}>
-              <View style={{alignItems: "center", }}>
+        <TouchableOpacity  onPress={()=>handleClick('male')}  style={[{},selectedGender=='male'?{borderRadius:10,padding:5,borderWidth: 4}:{}]}>
+              <View>
               <Image
                         style={{alignSelf:'center'}}
                         source={require('../../Assets/Images/male.png')}
                     />
               <Text  style={{  fontSize: 20,fontWeight: 'bold',textAlign:'center',color:'black'}}>Male</Text>
               </View>
-              <View style={{ alignItems: "center",borderWidth: 5, borderRadius:10 }}>
+          </TouchableOpacity>  
+          <TouchableOpacity  onPress={()=>handleClick('female')} style={[{},selectedGender=='female'?{borderWidth: 4, borderRadius:10,padding:5}:{}]}>  
+              <View>
               <Image
                         style={{alignSelf:'center'}}
                         source={require('../../Assets/Images/male.png')}
                     />
-                     <Text  style={{  fontSize: 20,fontWeight: 'bold',textAlign:'center',color:'black'}}>Female</Text>
+                     <Text  style={[{  fontSize: 20,fontWeight: 'bold',textAlign:'center',color:'black'}]}>Female</Text>
               </View>
+              </TouchableOpacity>
         </View>
         <TouchableOpacity style={styles.button}  >
       <Text style={{fontSize:20}}  >Continue</Text>
